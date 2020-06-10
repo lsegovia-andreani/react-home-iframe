e24ebc53b5440fab8b44e14d8fae04c23c5c268f
Leonardo Segovia <lsegovia@andreani.com>
2020-06-10 00:47:43 -0300

    Iframe same hostname diff port

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  var iframeUrl = "http://" + window.location.hostname + ":3001/"
   return (
     <div className="App">
       <p>Home</p>
       <div id="iframe-container">
-        <iframe title="Integration" id="iframe-integration" src="http://localhost:3001" width="95%" height="800px"></iframe>
+        <iframe title="Integration" id="iframe-integration" src={iframeUrl} width="95%" height="800px"></iframe>
       </div>
 
     </div>
