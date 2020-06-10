f87706b1d1ed23c2e2c23d70cf9cb3938e107b01
Leonardo Segovia <lsegovia@andreani.com>
2020-06-09 21:28:21 -0300

    Add home with iframe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Home</p>
+      <div id="iframe-container">
+        <iframe title="Integration" id="iframe-integration" src="http://localhost:3001" width="95%" height="800px"></iframe>
+      </div>
+
     </div>
   );
 }
